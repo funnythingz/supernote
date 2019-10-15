cfb555c476a71d88e818c32db650cabc4fa5f75e
funnythingz <funnythingz69+github@gmail.com>
2019-10-15 14:26:50 +0900

    to pug

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,24 @@
-<template>
-  <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
-          >
-        </a>
+<template lang="pug">
+  div
+    nav.navbar.header.has-shadow.is-primary(role="navigation" aria-label="main navigation")
+      .navbar-brand
+        a.navbar-item(href="/")
+          img(src="~assets/buefy.png" alt="Buefy" height="28")
+        .navbar-burger
+          span
+          span
+          span
 
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-    </section>
-  </div>
+    section.main-content.columns
+      aside.column.is-2.section
+        p.menu-label.is-hidden-touch
+        ul.menu-list
+          li(v-for="(item, key) of items" :key="key")
+            nuxt-link(:to="item.to" exact-active-class="is-active")
+              b-icon(:icon="item.icon")
+                | {{ item.title }}
+      .container.column.is-10
+        nuxt
 </template>
 
 <script>
